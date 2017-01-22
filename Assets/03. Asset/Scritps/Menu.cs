8f69c196d173680f7c888b6b639e4e7d67e5a91b
tsgarcia <thamires.luz@outlook.com>
2017-01-22 16:45:48 -0200

    first commit

---
+++ Assets/03. Asset/Scritps/Menu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+	public Button Play;
+	public Button Exit;
+
+	public void PlayGame(){
+		SceneManager.LoadScene (1);
+	}
+
+}
