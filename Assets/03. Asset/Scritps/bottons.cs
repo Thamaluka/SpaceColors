80186285e64edc2b0879668d74d97ab0afd14067
tsgarcia <thamires.luz@outlook.com>
2017-02-03 16:52:45 -0200

    Bottons

---
+++ Assets/03. Asset/Scritps/bottons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class bottons : MonoBehaviour {
+	public int idBotton;
+	public gameController gameControler;
+
+	void OnMouseDown(){
+		if(gameControler.gameState == GameState.RESPONDER){
+			gameControler.StartCoroutine ("responder", idBotton);
+	}
+}
+
+}
---
+++ Assets/03. Asset/Scritps/gameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState{
+	SEQUENCIA,
+	RESPONDER,
+	NOVA,
+	ERRO
+}
+
+public class gameController : MonoBehaviour {
+	public GameState gameState;
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("Responder");
+	}
+		
+		
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
